28c92644466665001bbfaad626b3e8782705c643
Julia <z3211232001@gmail.com>
2025-04-14 11:36:31 -0700

    fixed route name, end point to getUserById and change database name

--- backend/src/controllers/user.controller.ts
+++ backend/src/controllers/user.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import { User } from "../models/user.model";
 import bcrypt from "bcrypt";
+import { error } from "console";
 
 // get all users
 const getUsers = async (req: Request, res: Response) => {
@@ -12,6 +13,20 @@ const getUsers = async (req: Request, res: Response) => {
   }
 };
 
+//get user by id
+const getUserById = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  try {
+    const user = await User.findById(id)
+    if (!user) {
+      res.status(404).json({ error : "User not found"})
+      return
+    }
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(500).json({ error: "Something went wrong" });
+  }
+}
 // get user by email
 const getUserByEmail = async (req: Request, res: Response) => {
   const { email } = req.body;
@@ -109,6 +124,7 @@ const checkCookie = (req: Request, res: Response) => {
 
 export default {
   getUsers,
+  getUserById,
   getUserByEmail,
   loginUser,
   addUser,
--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -4,9 +4,9 @@ import userController from "../controllers/user.controller";
 const userRouter = Router()
 
 userRouter.get('/', userController.getUsers)
-userRouter.get('/check-auth', userController.getUserByEmail)
+userRouter.get('/:id', userController.getUserById)
 userRouter.post('/login', userController.loginUser)
-userRouter.post('/singup', userController.addUser)
+userRouter.post('/signup', userController.addUser)
 userRouter.get('/logout', userController.logout)
 userRouter.get('/check-cookie', userController.checkCookie)
 
