6c2f46e0d1200bf9cea8ba2a3967879327e90920
mizukifujikawa-ciccc <mz.fujikawa.ciccc@gmail.com>
2025-04-14 10:48:29 -0700

    Revert "Revert "login logout function""

---
+++ backend/src/models/user.model.ts
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose'
+
+const UserSchema = new mongoose.Schema({
+  firstName: { type: String, required: true },
+  lastName:  { type: String, required: true },
+  email:     { type: String, required: true, unique: true },
+  password:  { type: String }, // Google 用戶登入可以不設密碼
+  // authProvider: {
+  //   type: String,
+  //   enum: ['local', 'google'],
+  //   default: 'local',
+  //   required: true
+  // },
+}, { timestamps: true })
+
+export const User = mongoose.model('User', UserSchema)
