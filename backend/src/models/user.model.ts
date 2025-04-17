320f1a01862ee4f7c21e6265dadd53933cbd042c
mizukifujikawa-ciccc <mz.fujikawa.ciccc@gmail.com>
2025-04-17 12:46:44 -0700

    add img url to user

--- backend/src/models/user.model.ts
+++ backend/src/models/user.model.ts
@@ -6,6 +6,7 @@ const UserSchema = new Schema({
   lastName:  { type: String, required: true },
   email:     { type: String, required: true, unique: true },
   password:  { type: String, required: true },
+  imgUrl:    { type: String }
 }, { timestamps: true })
 
 const getUserByEmail = async (email: string) => {
